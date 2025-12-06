3b379a7d67c26a889bc1589a741c2bf8b48c60be
Shrinivas Venkatesan <venkat97@purdue.edu>
2025-12-05 23:04:14 -0500

    working

--- scholar.sh
+++ scholar.sh
@@ -3,13 +3,13 @@
 
 #SBATCH --export=ALL          # Export your current environment settings to the job environment
 #SBATCH --ntasks=1            # Number of MPI ranks per node (one rank per GPU)
-#SBATCH --gres=gpu:2          # Use one GPU
+#SBATCH --gres=gpu:1          # Use one GPU
 #SBATCH --mem-per-cpu=4G      # Required memory per GPU (specify how many GB)
 #SBATCH --time=4:00:00        # Total run time limit (hh:mm:ss)
 #SBATCH -J hmc                # Job name
 #SBATCH -o slurm_logs/%j      # Name of stdout output file
 #SBATCH --account=gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-gpu=32
 
@@ -24,7 +24,7 @@ cd /home/venkat97/cs593-cvd/ArtStyleToArtStyle
 python train_baseline.py  --root datasets \
   --dataset_name cezanne2photo \
   --model_type baseline \
-  --batch_size 2 \
+  --batch_size 1 \
   --num_workers 4 \
   --load_size 150 \
   --crop_size 128 \
